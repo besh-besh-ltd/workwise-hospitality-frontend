6b2ba5b76cdae2b69612a616566bc00739987a6e
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-02 16:44:34 +0530

    fix: fixed vendor product slug sitemap page api end point

--- pages/api/sitemap-vendors.xml.js
+++ pages/api/sitemap-vendors.xml.js
@@ -1,13 +1,12 @@
 // Assume you fetch vendor data dynamically here
 const EXTERNAL_DATA_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://letsworkwise.com';
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.letsworkwise.com'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.letsworkwise.com/api/v1'
 import axios from "axios";
 
 const getVendors = async () => {
   // Fetch your vendor data from the database
-  const response = await axios.get(`http://localhost:8002/api/v1/admin/sitemap/vendor-list`)
+  const response = await axios.get(`${API_URL}/admin/seo/products/slug`)
     .then((data) => {
-      console.log("i am executed")
       return data.data
     })
     .catch((error) => {
@@ -32,8 +31,8 @@ const createSitemap = (urls) => `<?xml version="1.0" encoding="UTF-8"?>
 export default async function sitemapVendorsXml(req, res) {
   const vendors = await getVendors();
   console.log(vendors?.[0])
-  const urls = vendors?.map(vendor => ({
-    loc: `/dashboard/buyer/rfq-management-vendor/vendor-profile?id=${vendor.id}`,
+  const urls = vendors?.map(slug => ({
+    loc: `/vendor/${slug}`,
     changefreq: 'weekly',
     priority: 0.5
   }));
