97b0f019fb939d23a9fad0a766f71c2877a721e2
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-22 18:43:13 +0530

    fix: added gtm in header of each page

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ import { Providers } from "@/redux/provider";
 import LogRocket from 'logrocket';
 import storageInstance from "@/utils/storageInstance";
 import Script from "next/script";
-import { GoogleTagManager } from "@next/third-parties/google";
+import Head from "next/head";
 
 
 // Tell Font Awesome to skip adding the CSS automatically
@@ -76,11 +76,18 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Only load Google tag managerin production */}
-      {process.env.NEXT_PUBLIC_ENV === "production" && (
-        <GoogleTagManager
-          gtmid={`${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
-        />
-      )}
+      <Head>
+  {process.env.NEXT_PUBLIC_ENV == "production" && (
+    <>
+      {/* Google Tag Manager Script */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID});`,
+        }}
+      />
+    </>
+  )}
+</Head>
 
       <ToastContainer style={{ zIndex: 10000 }} />
       {loading && <Loader />}
@@ -88,7 +95,7 @@ export default function App({ Component, pageProps }) {
         <GoogleOAuthProvider clientId="866474332918-fi599o8btdrikvi9ieq7pqksngvh2mlv.apps.googleusercontent.com">
           <Layout>
             {/* Only load Google Analytics and tag manager script in production */}
-            {process.env.NEXT_PUBLIC_ENV === 'production' && (
+            {process.env.NEXT_PUBLIC_ENV == 'production' && (
               <>
                 {/* <!-- Google Tag Manager (noscript) --> */}
                 <noscript>
