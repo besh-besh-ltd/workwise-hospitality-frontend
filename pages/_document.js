929d7b8ae37964389ba85b97c282a1ce8878724e
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-23 15:46:52 +0530

    refactor: move GTM _app.js to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,47 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+      {/* Only load Google tag managerin production */}
+      <Head>
+        {process.env.NEXT_PUBLIC_ENV == "production" && (
+          <>
+            {/* Google Tag Manager Script */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID});`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+      
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      ></link>
+
       <body>
+        {process.env.NEXT_PUBLIC_ENV == "production" && (
+          <>
+            {/* <!-- Google Tag Manager (noscript) --> */}
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              ></iframe>
+            </noscript>
+            {/* <!-- End Google Tag Manager (noscript) --> */}
+          </>
+        )}
+
         <Main />
         <NextScript />
       </body>
+      
     </Html>
-  )
+  );
 }
