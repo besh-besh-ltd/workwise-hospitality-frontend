695f31aed5e31973f227df3dcd111f4086423437
mukulsomukesh <mukulsomukesh@gmail.com>
2024-10-29 13:00:10 +0530

    shore: added meta tags in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,18 @@ export default function Document() {
     <Html lang="en">
       {/* Only load Google tag managerin production */}
       <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keywords"
+          content="procurement, tender, rfq, buyer, vendor"
+        ></meta>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link rel="icon" href="/fabicon.ico" />
+
         {process.env.NEXT_PUBLIC_ENV == "production" && (
           <>
             {/* Google Tag Manager Script */}
