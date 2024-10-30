4246c965f5b282ca765e67823a9b97dbd9645a24
SK Imtiaj Uddin <imtiaj@letsworkwise.com>
2024-10-30 20:24:28 +0530

    fix: Added og tags in public pages, changed schema tag in home page.

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 const pageInfo = {
     title: "Workwise: Find Approved Vendors, Automate RFQs, Compare Rates",
-    description: "Workwise saves 5% on costs and 90% on time in procurement, and helps you win double the tenders. Work wisely with master vendor database, automate RFQs & rate comparisons"
+    description: "Workwise saves 5% on costs and 90% on time in procurement, and helps you win double the tenders. Work wisely with master vendor database, automate RFQs & rate comparisons",
+    img:"https://letsworkwise.com/_next/image?url=https%3A%2F%2Fapi.letsworkwise.com%2Fbanner_image%2F1722515545528-100eb1f7-5df2-4b2b-8ab6-5916654bdd67.jpg"
 }
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>
-                    {pageInfo.title}
-                </title>
-                <meta
-                    name="description"
-                    content={pageInfo.description}
-                />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/fabicon.ico" />
+                <title> {pageInfo.title} </title>
+                <meta name="description" content={pageInfo.description} />
 
                 <script type="application/ld+json">
                     {JSON.stringify({
@@ -72,24 +66,32 @@ export default function Home() {
                             "contactType": "Customer Service",
                             "email": "hello@letsworkwise.com",
                             "telephone": "9930787798",
-                            "hoursAvailable": {
-                                "@type": "OpeningHoursSpecification",
-                                "name": "24/7",
-                                "address": {
-                                    "@type": "PostalAddress",
-                                    "streetAddress": "Jbr Tech Park, Whitefield, Bengaluru, Karnataka 560066",
-                                    "addressLocality": "Bengaluru",
-                                    "addressRegion": "Karnataka",
-                                    "postalCode": "560066",
-                                    "addressCountry": {
-                                        "@type": "Country",
-                                        "name": "India"
-                                    }
+                            "hoursAvailable": [
+                                {
+                                    "@type": "OpeningHoursSpecification",
+                                    "name": "24/7",
+                                    "dayOfWeek": [
+                                        "https://schema.org/Monday",
+                                        "https://schema.org/Tuesday",
+                                        "https://schema.org/Wednesday",
+                                        "https://schema.org/Thursday",
+                                        "https://schema.org/Friday",
+                                        "https://schema.org/Saturday"
+                                    ],
+                                    "opens": "10:00",
+                                    "closes": "20:00"
                                 }
-                            }
+                            ]
                         }
                     })}
                 </script>
+
+                <meta property="og:title" content={pageInfo.title} />
+                <meta property="og:description" content={pageInfo.description} />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={`${process.env.NEXT_PUBLIC_FRONTEND_URL}`} />
+                <meta property="og:image" content={pageInfo.img} />
+                <meta property="og:site_name" content="Workwise" />
             </Head>
             <Homepage />
         </>
