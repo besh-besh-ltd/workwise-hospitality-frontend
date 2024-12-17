2af6e21cb7366de7658611506e64aaf2279b163c
SK Imtiaj Uddin <imtiaj@letsworkwise.com>
2024-12-17 17:18:34 +0530

    enhancement: added project filter in RFQ overview, fixed monthly filters where current month data was not coming and added fixed height in vendor section.

--- components/dashboard/buyer/dashboard-components/RfqOverview.js
+++ components/dashboard/buyer/dashboard-components/RfqOverview.js
@@ -3,17 +3,20 @@ import Utils from '@/components/shared/ChartConfig/utils';
 import { faArrowRight, faBell, faCartPlus, faCheckToSlot, faRectangleXmark, faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import AsyncSelect from 'react-select/async';
 import Select from 'react-select';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 import FullLoader from '@/components/shared/FullLoader';
 import { getRfqChartData, sendReminder } from '@/services/rfq';
 import { formatDate } from "@/utils/sharedFunctions";
+import { getProjectList } from '@/services/project';
 
 const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
     const [rfqData, setRfqData] = useState(null);
     const [chartData, setChartData] = useState(null);
     const [chartTitle, setChartTitle] = useState('');
+    const [selectedProject, setSelectedProject] = useState(null);
     const [filter, setFilter] = useState({ label: 'Last 7 days', value: 'past7days' });
     const [chartType, setChartType] = useState({ label: 'Cubic Line Chart', value: 'cubic' });
     const [loading, setLoading] = useState(false);
@@ -22,7 +25,10 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
     const getChartData = async () => {
         setLoading(true);
         try {
-            const res = await getRfqChartData(filter.value);
+            const res = await getRfqChartData(
+                filter.value,
+                selectedProject ? selectedProject.value : null
+            );
             generateChartData(res.data);
         } catch (error) {
             toast.error(error.message);
@@ -31,123 +37,103 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
         }
     }
 
+    const getAllProjects = async () => {
+        try {
+            const res = await getProjectList();
+            return res.data.map((item) => (
+                { label: item.name, value: item.id }
+            ));
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const handleChange = (selectedOption, actionMeta) => {
         const { name } = actionMeta;
-        if (name == 'date_range')
-            setFilter(selectedOption);
-        else
-            setChartType(selectedOption);
+        switch (name) {
+            case 'date_range':
+                setFilter(selectedOption);
+                break;
+            case 'chart_type':
+                setChartType(selectedOption);
+                break;
+            case 'project_select':
+                setSelectedProject(selectedOption);
+                break;
+            default:
+                break;
+        }
     }
 
     const generateChartData = (api_data) => {
         const title = Utils.CHART_TITLE({ labelType: filter.value });
         const { result: actualDates, labels: fullDateRange } = Utils.DATE_RANGE({ rangeType: filter.value });
 
-        const dateDataMap = actualDates.reduce((acc, date) => {
-            acc[date] = {
-                new_rfqs: 0,
-                closed_rfqs: 0,
-                completed_rfqs: 0,
-                quotes_received: 0,
-            };
+        const sortedDates = actualDates.sort((a, b) => new Date(a) - new Date(b));
+
+        const dateDataMap = sortedDates.reduce((acc, date) => {
+            acc[date] = { new_rfqs: 0, closed_rfqs: 0, completed_rfqs: 0, quotes_received: 0 };
             return acc;
         }, {});
 
-        let total_new_rfqs = 0;
-        let total_closed_rfqs = 0;
-        let total_completed_rfqs = 0;
-        let total_quotes_received = 0;
+        let totals = { new_rfqs: 0, closed_rfqs: 0, completed_rfqs: 0, quotes_received: 0 };
 
+        const isMonthly = ['past3months', 'past6months', 'wholeYear'].includes(filter.value);
 
-        if (filter.value === 'past3months' || filter.value === 'past6months' || filter.value === 'wholeYear') {
-            // Monthly data
-            api_data.forEach(item => {
-                const formattedMonth = new Date(`${item.month}-01`).toLocaleDateString('en-IN');
-                if (dateDataMap[formattedMonth]) {
-                    dateDataMap[formattedMonth] = {
-                        new_rfqs: parseInt(item.new_rfqs, 10),
-                        closed_rfqs: parseInt(item.closed_rfqs, 10),
-                        completed_rfqs: parseInt(item.completed_rfqs, 10),
-                        quotes_received: parseInt(item.quotes_received, 10),
-                    };
+        api_data.forEach(item => {
+            const formattedPeriod = isMonthly
+                ? new Date(`${item.date}-01`).toLocaleDateString('en-IN')
+                : new Date(item.date).toLocaleDateString('en-IN');
 
-                    total_new_rfqs += parseInt(item.new_rfqs);
-                    total_closed_rfqs += parseInt(item.closed_rfqs);
-                    total_completed_rfqs += parseInt(item.completed_rfqs);
-                    total_quotes_received += parseInt(item.quotes_received);
-                }
-            });
-        } else {
-            // Date Wise data
-            api_data.forEach(item => {
-                const formattedDate = new Date(item.date).toLocaleDateString('en-IN');
-                if (dateDataMap[formattedDate]) {
-                    dateDataMap[formattedDate] = {
-                        new_rfqs: parseInt(item.new_rfqs, 10),
-                        closed_rfqs: parseInt(item.closed_rfqs, 10),
-                        completed_rfqs: parseInt(item.completed_rfqs, 10),
-                        quotes_received: parseInt(item.quotes_received, 10),
-                    };
+            if (dateDataMap[formattedPeriod]) {
+                const new_rfqs = parseInt(item.new_rfqs, 10);
+                const closed_rfqs = parseInt(item.closed_rfqs, 10);
+                const completed_rfqs = parseInt(item.completed_rfqs, 10);
+                const quotes_received = parseInt(item.quotes_received, 10);
 
-                    total_new_rfqs += parseInt(item.new_rfqs);
-                    total_closed_rfqs += parseInt(item.closed_rfqs);
-                    total_completed_rfqs += parseInt(item.completed_rfqs);
-                    total_quotes_received += parseInt(item.quotes_received);
-                }
-            });
-        }
+                dateDataMap[formattedPeriod] = { new_rfqs, closed_rfqs, completed_rfqs, quotes_received };
+
+                totals.new_rfqs += new_rfqs;
+                totals.closed_rfqs += closed_rfqs;
+                totals.completed_rfqs += completed_rfqs;
+                totals.quotes_received += quotes_received;
+            }
+        });
 
         const labels = Object.keys(dateDataMap);
-        const preparedData = {
-            new_rfqs: labels.map(label => dateDataMap[label].new_rfqs),
-            closed_rfqs: labels.map(label => dateDataMap[label].closed_rfqs),
-            completed_rfqs: labels.map(label => dateDataMap[label].completed_rfqs),
-            quotes_received: labels.map(label => dateDataMap[label].quotes_received),
-        };
+        const preparedData = ['new_rfqs', 'closed_rfqs', 'completed_rfqs', 'quotes_received'].reduce((acc, key) => {
+            acc[key] = labels.map(label => dateDataMap[label][key]);
+            return acc;
+        }, {});
 
-        let dataSets = [
+        const dataSets = [
             {
                 label: 'New RFQs',
                 data: preparedData.new_rfqs,
                 backgroundColor: Utils.CHART_COLORS.blue,
-                borderColor: Utils.CHART_COLORS.blue,
-                fill: false,
+                borderColor: Utils.CHART_COLORS.blue
             },
             {
                 label: 'Closed RFQs',
                 data: preparedData.closed_rfqs,
                 backgroundColor: Utils.CHART_COLORS.red,
-                borderColor: Utils.CHART_COLORS.red,
-                fill: false,
+                borderColor: Utils.CHART_COLORS.red
             },
             {
                 label: 'Completed RFQs',
                 data: preparedData.completed_rfqs,
                 backgroundColor: Utils.CHART_COLORS.green,
-                borderColor: Utils.CHART_COLORS.green,
-                fill: false,
+                borderColor: Utils.CHART_COLORS.green
             },
             {
                 label: 'Quotes Received',
                 data: preparedData.quotes_received,
                 backgroundColor: Utils.CHART_COLORS.yellow,
-                borderColor: Utils.CHART_COLORS.yellow,
-                fill: false,
+                borderColor: Utils.CHART_COLORS.yellow
             },
-        ]
+        ].map(item => (chartType.value === 'cubic' ? { ...item, tension: 0.4 } : { ...item, fill: false }));
 
-        if (chartType.value === 'cubic') {
-            dataSets = dataSets.map((item) => (
-                { ...item, tension: 0.4 }
-            ))
-        }
-
-        setRfqData({
-            total_new_rfqs,
-            total_closed_rfqs,
-            total_completed_rfqs,
-            total_quotes_received,
-        })
+        setRfqData(totals);
         setChartTitle(title);
         setChartData({
             labels: fullDateRange,
@@ -182,7 +168,7 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
 
     useEffect(() => {
         getChartData();
-    }, [filter])
+    }, [filter, selectedProject])
 
     useEffect(() => {
         if (chartData) {
@@ -221,13 +207,16 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                 {/* RFQ Overview */}
                 <div className="overview-container col-md-3 pe-2">
                     <div className="rounded-2 shadow p-4 h-100 hasFullLoader">
-                        <h2 className="fs-4 fw-medium">RFQ Overview</h2>
+                        <h2 className="fs-4 fw-medium mb-1">RFQ Overview</h2>
                         {loading && <FullLoader />}
+
+                        {selectedProject && <p className="text-sm fw-semibold mb-0" >{selectedProject.label}</p>}
+                        <p className="text-sm fw-medium" >{chartTitle.split(/from |for the |for /)[1]}</p>
                         <div className="d-flex flex-column justify-content-between gap-2">
                             <div className="border border-dark rounded-4 px-4 py-3 opacity-75">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h3 className="fs-4 fw-medium mb-0">
-                                        {rfqData ? (rfqData.total_new_rfqs || 0) : 0}
+                                        {rfqData ? (rfqData.new_rfqs || 0) : 0}
                                         <span className="d-block fs-6">New RFQs</span>
                                     </h3>
                                     <FontAwesomeIcon icon={faCartPlus} fontSize={28} />
@@ -237,7 +226,7 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                             <div className="border border-dark rounded-4 px-4 py-3 opacity-75">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h3 className="fs-4 fw-medium mb-0">
-                                        {rfqData ? (rfqData.total_quotes_received || 0) : 0}
+                                        {rfqData ? (rfqData.quotes_received || 0) : 0}
                                         <span className="d-block fs-6">Quotes Received</span>
                                     </h3>
                                     <FontAwesomeIcon icon={faStopwatch} fontSize={28} />
@@ -247,7 +236,7 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                             <div className="border border-dark rounded-4 px-4 py-3 opacity-75">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h3 className="fs-4 fw-medium mb-0">
-                                        {rfqData ? (rfqData.total_completed_rfqs || 0) : 0}
+                                        {rfqData ? (rfqData.completed_rfqs || 0) : 0}
                                         <span className="d-block fs-6">Completed RFQs</span>
                                     </h3>
                                     <FontAwesomeIcon icon={faCheckToSlot} fontSize={28} />
@@ -257,7 +246,7 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                             <div className="border border-dark rounded-4 px-4 py-3 opacity-75">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h3 className="fs-4 fw-medium mb-0">
-                                        {rfqData ? (rfqData.total_closed_rfqs || 0) : 0}
+                                        {rfqData ? (rfqData.closed_rfqs || 0) : 0}
                                         <span className="d-block fs-6">Closed RFQs</span>
                                     </h3>
                                     <FontAwesomeIcon icon={faRectangleXmark} fontSize={28} />
@@ -283,7 +272,20 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                         <div className="d-flex justify-content-between align-items-center mb-4">
                             <h3 className="fs-4 fw-medium mb-0">RFQ Stats</h3>
 
-                            <div className="col-md-5 d-flex justify-content-between gap-2">
+                            <div className="col-md-8 d-flex justify-content-between gap-2">
+                                <AsyncSelect
+                                    cacheOptions
+                                    loadOptions={getAllProjects}
+                                    defaultOptions
+                                    name="project_select"
+                                    className="text-sm w-100 mb-2"
+                                    onChange={handleChange}
+                                    value={selectedProject}
+                                    placeholder="Choose Project"
+                                    isClearable
+                                    noOptionsMessage={() => "No Projects Found"}
+                                />
+
                                 <Select
                                     options={[
                                         { label: 'Cubic Line Chart', value: 'cubic' },
@@ -461,22 +463,17 @@ const RfqOverview = ({ tableRfqData, notificationData, tableLoading }) => {
                 <div className="notification-container col-md-4 ps-2">
                     <div className="bg-white d-flex flex-column shadow rounded-2 p-4 h-100">
                         <h2 className="fs-4 fw-medium  ">Notifications</h2>
-                        <div className="notification-section list-group h-100 overflow-auto"
-                            style={{ maxHeight: "450px" }}
-                        >
+                        <div className="notification-section list-group h-100 overflow-y-auto" style={{ maxHeight: "450px" }}>
                             {notificationData && notificationData.length > 0
                                 ? notificationData.map((notification) => (
                                     <div class="card mb-1">
                                         <div class="card-body p-2">
-                                            <h5 class="card-title my-1" style={{ fontSize: "16px", fontWeight: "600" }}>{notification?.notification_type == 'rfq_created'
-                                              ? "New RFQ created" : "New Quotation Received"}</h5>
-                                            <p class="card-text mb-0" style={{ fontSize: "16px", fontWeight: "400" }}>{notification?.notification_type == 'rfq_created'
-                                              ? `You created a new RFQ #${notification?.rfq_no} and shared it with the vendors.`
-                                              : ` ${notification?.organization_name || notification?.vendor_name} has sent you a new quotation on RFQ #${notification?.rfq_no}.`}</p>
-                                            {/* <span className="badge bg-primary rounded-pill">
-                                                {formatDate(notification?.timestamp)}
-                                            </span> */}
-                                            <p className='m-0' style={{ fontSize: "14px", fontWeight: "200", color: "grey" }}>{formatDate(notification?.readable_date_time)}</p>
+                                            <h3 class="card-title fs-6 fw-semibold my-1">{notification?.notification_type == 'rfq_created'
+                                                ? "New RFQ created" : "New Quotation Received"}</h3>
+                                            <p class="card-text text-sm mb-0">{notification?.notification_type == 'rfq_created'
+                                                ? `You created a new RFQ #${notification?.rfq_no} and shared it with the vendors.`
+                                                : ` ${notification?.organization_name || notification?.vendor_name} has sent you a new quotation on RFQ #${notification?.rfq_no}.`}</p>
+                                            <span className='d-block m-0 fw-semibold text-end' style={{ fontSize: "12px", color: "grey" }}>{formatDate(notification?.readable_date_time)}</span>
                                         </div>
                                     </div>
                                 ))
