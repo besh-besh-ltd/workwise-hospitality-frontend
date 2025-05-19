036acdb7b76c75bbfd23da01cbdde7740fb17f3b
agnij-workwise <agnij@letsworkwise.com>
2025-05-19 18:09:09 +0530

    refactor: Remove index files for account and project management components

--- components/dashboard/admin/account-management/index.js
+++
@@ -1,3 +0,0 @@
-// Export all account management components
-export { default as ManageAccountsPage } from './manage-accounts';
-export { default as EditAccountModal } from './EditAccountModal';
--- components/dashboard/admin/project-management/index.js
+++
@@ -1,6 +0,0 @@
-// Export all project management components
-export { default as ProjectManagementPage } from './project-management';
-export { default as ProjectDetailsPage } from './project-details';
-export { default as CreateProjectModal } from './CreateProjectModal';
-export { default as EditProjectModal } from './EditProjectModal';
-export { default as AddTeamMemberModal } from './AddTeamMemberModal';
