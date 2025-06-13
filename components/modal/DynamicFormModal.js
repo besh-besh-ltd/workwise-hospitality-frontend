cf595404f6622292d3bd77a12e28d9bb973fa5cf
mukulsomukesh <mukulsomukesh@gmail.com>
2025-06-14 02:11:38 +0530

    fix: Remove unused status field from DynamicFormModal for cleaner code fix: Remove Admin role option from roleOptions in ManageAccountsPage

--- components/modal/DynamicFormModal.js
+++ components/modal/DynamicFormModal.js
@@ -986,20 +986,7 @@ Example:
                                     )}
                                 </div>
                                 
-                                {type === "edit-project" && (
-                                  <div className="form-group">
-                                    <label htmlFor="status">Status</label>
-                                    <Field
-                                      as="select"
-                                      id="status"
-                                      name="status"
-                                      className="form-control"
-                                    >
-                                      <option value={1}>Active</option>
-                                      <option value={0}>Inactive</option>
-                                    </Field>
-                                  </div>
-                                )}
+
                               </>
                             )}
                           </div>
