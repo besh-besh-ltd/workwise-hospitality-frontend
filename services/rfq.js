56f5c8dbf04d2deed9fb84b611cd657a3a6a4453
ayush-workwise <ayush@letsworkwise.com>
2025-07-18 19:21:41 +0530

    Enhance: Update vendorApproveList to include variant_id as a query parameter

--- services/rfq.js
+++ services/rfq.js
@@ -33,7 +33,7 @@ export const vendorApproveList = (values) => {
  
   return new Promise(async (resolve, reject) => {
     try {
-      let response = await axiosInstance.get(`/users/vendorapprove-list`);
+      let response = await axiosInstance.get(`/users/vendorapprove-list`, { params: {variant_id: values }});
       resolve(response);
     } catch (error) {
       reject({ message: error });
