0a6a4f9da897dbce23b365cd94ce3b8b25ab6e17
agnij-workwise <agnij@letsworkwise.com>
2026-01-01 18:59:34 +0530

    fix: add window check in storageInstance methods to prevent errors in non-browser environments

--- utils/storageInstance.js
+++ utils/storageInstance.js
@@ -2,9 +2,11 @@ import { reactLocalStorage } from 'reactjs-localstorage'
 
 class storageInstance {
   static setStorage(key, value) {
+    if (typeof window === 'undefined') return null
     return reactLocalStorage.set(key, value)
   }
   static setStorageObj(key, value) {
+    if (typeof window === 'undefined') return null
     // const encrypkey = process.env.ENCRYPTION_KEY || ''
     // const encryptedValue = CryptoJS.AES.encrypt(
     //   JSON.stringify(value),
@@ -14,6 +16,7 @@ class storageInstance {
   }
 
   static getStorage(key) {
+    if (typeof window === 'undefined') return null
     if (reactLocalStorage.get(key)) {
         //const encrypkey = process.env.ENCRYPTION_KEY || ''
     //   const bytes = CryptoJS.AES.decrypt(reactLocalStorage.get(key), encrypkey)
@@ -26,6 +29,7 @@ class storageInstance {
   }
   
   static getStorageObj(key) {
+    if (typeof window === 'undefined') return ''
     const encrypkey = process.env.ENCRYPTION_KEY || ''
     if (reactLocalStorage.get(key)) {
         // const encrypkey = process.env.ENCRYPTION_KEY || ''
@@ -39,10 +43,12 @@ class storageInstance {
   }
 
   static removeStorege(key) {
+    if (typeof window === 'undefined') return null
     return reactLocalStorage.remove(key)
   }
 
   static clearStorege() {
+    if (typeof window === 'undefined') return
     reactLocalStorage.clear()
   }
 }
