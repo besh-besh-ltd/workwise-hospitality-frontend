88ea161e06decde9e489b08ee4e30801ddc6c07f
sarvesh-ctrl <sarvesh@letsworkwise.com>
2026-06-18 13:38:05 +0530

    refactor: remove ExistingPOModal and related logic from approval workflow

--- services/rfq.js
+++ services/rfq.js
@@ -561,22 +561,6 @@ export const getLastPurchaseDetails = (payload) => {
   });
 };
 
-export const getExistingPOByVendor = (vendor_id, rfq_id) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await axiosInstance.get('/rfq/get-existing-po', {
-        params: {
-          vendor_id,
-          rfq_id
-        },
-      });
-      resolve(response);
-    } catch (error) {
-      reject({ message: error });
-    }
-  });
-};
-
 
 export const closeRFQ = (id, comment) => {
   return new Promise(async (resolve, reject) => {
